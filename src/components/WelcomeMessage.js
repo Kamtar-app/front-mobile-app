7288b2ce7b033d9a8944228ece64ec2e603af193
Manon <manon61550@gmail.com>
2023-12-29 17:26:33 +0100

    fix: alignItems

--- src/components/WelcomeMessage.js
+++ src/components/WelcomeMessage.js
@@ -1,37 +1,37 @@
 import {
-    StyleSheet,
-    Text,
-    View,
-    TextInput,
-    Image,
-    TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Image,
+  TouchableOpacity,
 } from "react-native";
 import { colors } from "../assets/styles/constants/colors";
 
 export const WelcomeMessage = ({ label }) => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.label}>Belle journée,</Text>
-            <Text style={styles.name}>Elsa</Text>
-        </View>
-    );
+  return (
+    <View style={styles.messageContainer}>
+      <Text style={styles.label}>Belle journée,</Text>
+      <Text style={styles.name}>Elsa</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        alignItems: "left",
-        justifyContent: "center",
-        marginTop: 37,
-        marginLeft: 20
-    },
-    label: {
-        fontSize: 25,
-        fontWeight: "bold",
-        color: colors.black
-    },
-    name: {
-        fontSize: 25,
-        fontWeight: "bold",
-        color: colors.primary
-    }
+  messageContainer: {
+    alignItems: "flex-start",
+    justifyContent: "center",
+    marginTop: 37,
+    marginLeft: 20,
+  },
+  label: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: colors.black,
+  },
+  name: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: colors.primary,
+  },
 });
